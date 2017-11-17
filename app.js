9fc547a0600f1314c24ca8bf0324410af3a8f4bb
Tevis Dryden <TevisDryden@gmail.com>
2017-11-17 13:36:44 -0700

    go2

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/comments');
+mongoose.connect('mongodb://localhost/pledges');
 require('./models/Comments');
 require('./models/Pledges');
 
