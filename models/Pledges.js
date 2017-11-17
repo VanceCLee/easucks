08f167139ff93a2b9e8924d24c7152c32836d6bc
Tevis Dryden <TevisDryden@gmail.com>
2017-11-16 23:44:04 -0700

    plz2

--- models/Pledges.js
+++ models/Pledges.js
@@ -4,7 +4,7 @@ var PledgeSchema = new mongoose.Schema({
   pledges: {type: Number, default: 0},
 });
 PledgeSchema.methods.pledge = function(cb) {
-  this.upvotes += 1;
+  this.pledges += 1;
   this.save(cb);
 };
 mongoose.model('Pledges', PledgeSchema);
